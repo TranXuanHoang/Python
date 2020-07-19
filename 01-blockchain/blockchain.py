ef161a4e5914c6830021c020f7126d595bbe7f18
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-20 00:00:44 +0900

    Improve the blockchain transaction process by an infinite loop

--- 01-blockchain/blockchain.py
+++ 01-blockchain/blockchain.py
@@ -4,10 +4,12 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Get the last block value from the blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
     """ Append a new transaction value as well as the last blockchain value
         to the blockchain.
 
@@ -15,22 +17,39 @@ def add_value(transaction_amount, last_transaction=[1]):
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction (default [1]).
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def print_blockchain_elements():
+    """ Print out all blocks in the blockchain. """
+    for block in blockchain:
+        print(block)
+
+
+def get_transaction_value():
     """ Return user input as a float. """
     return float(input('Your transaction amount please: '))
 
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    """ Return user choice as a string. """
+    return input('Your choice: ')
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(),
-          transaction_amount=tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-print(blockchain)
+while True:
+    print('Please choose an option')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Invalid option!')
