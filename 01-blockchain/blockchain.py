57d9a7f01eeeddd78d414cfa4d0e75b61aa08ba6
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-21 01:20:16 +0900

    Mine blocks and hash previous blocks

--- 01-blockchain/blockchain.py
+++ 01-blockchain/blockchain.py
@@ -1,6 +1,13 @@
+# Genesis block
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+
 # Initialize a blockchain as a list
-blockchain = []
-open_transaction = []
+blockchain = [genesis_block]
+open_transactions = []
 owner = 'Hoang'
 
 
@@ -25,11 +32,19 @@ def add_transaction(sender, recipient, amount=1.0):
         'recipient': recipient,
         'amount': amount
     }
-    open_transaction.append(transaction)
+    open_transactions.append(transaction)
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hashed_block = '-'.join([str(last_block[key]) for key in last_block])
+    print(hashed_block)
+    block = {
+        'previous_hash': hashed_block,
+        'index': len(blockchain),
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def print_blockchain_elements():
@@ -65,16 +80,19 @@ def verify_chain():
 while True:
     print('Please choose an option')
     print('1: Add a new transaction value')
-    print('2: Output the blockchain blocks')
-    print('h: Change the first block')
+    print('2: Mine a new block')
+    print('3: Output the blockchain blocks')
+    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_data = get_transaction_value()
         recipient, amount = tx_data
         add_transaction(owner, recipient, amount=amount)
-        print(open_transaction)
+        print(open_transactions)
     elif user_choice == '2':
+        mine_block()
+    elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == 'h':
         if len(blockchain) >= 1:
@@ -83,6 +101,6 @@ while True:
         break
     else:
         print('Invalid option!')
-    if not verify_chain():
-        print('Invalid blockchain!')
-        break
+    # if not verify_chain():
+    #     print('Invalid blockchain!')
+    #     break
