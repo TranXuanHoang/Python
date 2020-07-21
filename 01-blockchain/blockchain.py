82b1df4452c85f4a125d12b7d0324f490ab72f25
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-21 22:39:03 +0900

    Manage a list of participants in the blockchain

--- 01-blockchain/blockchain.py
+++ 01-blockchain/blockchain.py
@@ -9,6 +9,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Hoang'
+participants = set([owner])
 
 
 def hash_block(last_block):
@@ -42,6 +43,8 @@ def add_transaction(sender, recipient, amount=1.0):
         'amount': amount
     }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
@@ -97,6 +100,7 @@ while True:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -112,6 +116,8 @@ while True:
         mine_block()
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
