bb3980b19fda1b625379b11e4f4abe0a46d9eb08
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-22 16:59:02 +0900

    Define a new function to verify all open transactions

--- 01-blockchain/blockchain.py
+++ 01-blockchain/blockchain.py
@@ -151,12 +151,22 @@ def verify_chain():
     return True
 
 
+def verify_transactions():
+    """ Verify whether all open transactions are valid.
+
+    Returns:
+        True if all transactions are valid, False otherwise.
+    """
+    return all([verify_transaction(tx) for tx in open_transactions])
+
+
 while True:
     print('Please choose an option')
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
     print('4: Output participants')
+    print('5: Verify all transactions')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -178,6 +188,11 @@ while True:
         print_blockchain_elements()
     elif user_choice == '4':
         print(participants)
+    elif user_choice == '5':
+        if verify_transactions():
+            print('All transactions are valid')
+        else:
+            print('There are invalid transactions')
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = {
