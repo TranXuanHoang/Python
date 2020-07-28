8105e3d8183a2949b513fdfb1fe0aeada6344187
Tran Xuan Hoang <hoangtx.ict@gmail.com>
2020-07-28 22:04:14 +0900

    Add a GET API to fetch the blockchain
    * Endpoint: GET /chain

--- 01-blockchain/node.py
+++ 01-blockchain/node.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
+from blockchain import Blockchain
 from wallet import Wallet
 
 app = Flask(__name__)
 wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
 CORS(app)
 
 
 @app.route('/', methods=['GET'])
 def get_ui():
     return 'Blockchain app!'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    """ Get the entire blockchain. """
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__ for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [
+            tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
 
 
 if __name__ == '__main__':
